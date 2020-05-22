e487773ecfe076224aa00d85c200caaf44c96214
Katherine Ross <katylahv@gmail.com>
2020-05-22 18:26:55 -0500

    initialized Spring Boot Project

---
+++ src/main/java/com/ross/sidework/SideworkApplication.java
@@ -0,0 +1,13 @@
+package com.ross.sidework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ross/sidework/SideworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ross.sidework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

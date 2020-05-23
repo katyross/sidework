ba425882f802c0bc1f68b3fa34213b67afac2e46
Katherine Ross <katylahv@gmail.com>
2020-05-23 01:23:40 -0500

    created env variables for connection settings. connected db to spring

--- spring/src/main/java/com/ross/sidework/controllers/HomeController.java
+++ spring/src/main/java/com/ross/sidework/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.ross.sidework;
+package com.ross.sidework.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

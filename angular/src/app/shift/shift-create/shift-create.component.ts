06f9283db3f563eab05a891216094f1948ddd339
Katherine Ross <katylahv@gmail.com>
2020-06-28 21:19:46 -0500

    added shift-create module

---
+++ angular/src/app/shift/shift-create/shift-create.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit} from '@angular/core';
+import {Restaurant} from "../../restaurant/restaurant";
+import {Shift} from "../shift";
+import {ShiftService} from "../shift.service";
+import {Router} from "@angular/router";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'app-shift-create',
+  templateUrl: './shift-create.component.html',
+  styleUrls: ['./shift-create.component.css']
+})
+export class ShiftCreateComponent implements OnInit {
+  shift: Shift;
+  restaurantList: Restaurant[];
+
+  constructor(private shiftService: ShiftService,
+              private router: Router,
+              private activeModal: NgbActiveModal) {
+    this.shift = new Shift();
+    this.restaurantList = [];
+  }
+
+  ngOnInit() {
+    this.getRestaurants();
+  }
+  // save new object on submit
+    onSubmit(shift: any) {
+      // find restaurantWorked
+      this.shiftService.saveShift(shift)
+        .then(successResponse =>{
+          this.goToShiftList();
+        })
+        .catch(errorResponse => {
+        });
+      }
+
+  getRestaurants(): any{
+    this.shiftService.getAllRestaurants().then(successResponse => {
+      this.restaurantList = successResponse;
+    })
+      .catch(errorResponse => {
+
+      });
+  }
+
+  //go to the list of shifts once shift is submitted
+  goToShiftList(){
+    this.router.navigate(['/shifts']);
+  }
+}
